957e9d3b72e221555e3e56f4a20faa0d75f2a5a0
danilo <danilocastro81@gmail.com>
2016-01-11 17:49:56 -0200

    queries contants class

---
+++ pet-service/src/pet/constants/QueriesConstants.java
@@ -0,0 +1,8 @@
+package pet.constants;
+
+public class QueriesConstants {
+
+	public static final String RAZAO_SOCIAL_FIELD="razaoSocial";
+	public static final String OBJECT_ID_FIELD="_id";
+	public static final String CNPJ_CPF_FIELD="cpfCnpj";
+}
