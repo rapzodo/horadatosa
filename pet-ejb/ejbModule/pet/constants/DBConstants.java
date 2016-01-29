3285c62374f4a5fde78af074ccee273655c2487e
danilo <danilocastro81@gmail.com>
2016-01-29 17:30:12 -0200

    deleting other dummy projects

--- pet-ejb/ejbModule/pet/constants/DBConstants.java
+++
@@ -1,7 +0,0 @@
-package pet.constants;
-
-public class DBConstants {
-
-	public static final String DB_NAME="petdb";
-	public static final String ENTITIES_PKG="pet.mongo.morphia.entities";
-}
--- pet-service/src/com/pet/rest/AnimalCRUDService.java
+++ pet-service/src/com/pet/rest/AnimalCRUDService.java
@@ -8,8 +8,6 @@ import com.pet.mongo.morphia.entities.Animal;
 @Path("/animais")
 public class AnimalCRUDService extends BaseCrudService<Animal>{
 	
-	private BaseMongoDao<Animal> dao= new BaseMongoDao<>(Animal.class);
-	
 	public AnimalCRUDService(){
 		dao = new BaseMongoDao<>(Animal.class);
 	}
